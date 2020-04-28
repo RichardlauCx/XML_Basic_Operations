bc792cda7b21b7d487ab17fad56eddada357bb74
RichardlauCx <2409608770@qq.com>
2020-04-28 15:41:00 +0800

    Initial commit, And DOM operations

---
+++ src/com/company/DOM/DOM_oper.java
@@ -0,0 +1,15 @@
+package com.company.DOM;
+
+// -*- coding: utf-8 -*-
+//  @ Date   : 2019/5/20 13:14
+//  @ Author : RichardLau_Cx
+//  @ file   : Richard.Java
+//  @ IDE    : IDEA
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class DOM_oper {
+    // DOM 解析器
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+}
